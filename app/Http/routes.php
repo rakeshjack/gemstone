701c04cca9e0ddeecf9c1da87d4b55fefb278e4e
Rakesh <rakeshjack7@gmail.com>
2017-07-14 18:48:16 +0530

    GemStone Commit 1

---
+++ app/Http/routes.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| GEMSTONE Home Page
+|--------------------------------------------------------------------------
+*/
+Route::get('/', function () {
+    return view('gem/home/index');
+});
+/*
+|--------------------------------------------------------------------------
+| GEMSTONE Home Page
+|--------------------------------------------------------------------------
+*/
+Route::get('/about', function () {
+    return view('gem/about/index');
+});
+/*
+|--------------------------------------------------------------------------
+| GEMSTONE Gallery Page
+|--------------------------------------------------------------------------
+*/
+Route::get('/gallery', function () {
+    return view('gem/gallery/index');
+});
+/*
+|--------------------------------------------------------------------------
+| GEMSTONE Jewellery Page
+|--------------------------------------------------------------------------
+*/
+Route::get('/jewellery', function () {
+    return view('gem/jewelry/index');
+});
+/*
+|--------------------------------------------------------------------------
+| GEMSTONE Jewellery Page
+|--------------------------------------------------------------------------
+*/
+Route::get('/goldcoins', function () {
+    return view('gem/coins/index');
+});
